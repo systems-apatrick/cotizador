cc0db55b52ae0f08ec171390ac8ffb4992c59f01
Patricio Fajardo <apatricio.fajardoc@gmail.com>
2021-08-20 14:17:56 -0500

    Se agrega styled al componente Formulario.js

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,22 +1,45 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Campo = styled.div`
+  display: flex;
+  margin-bottom: 1rem;
+  align-items: center;
+`;
+
+const Label = styled.label`
+  flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+  display: block;
+  width: 100%;
+  padding: 1rem;
+  border: 1px solid #e1e1e1;
+  -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+  margin: 0 1rem;
+`;
 
 const Formulario = () => {
   return (
     <form>
-      <div>
-        <label>Marca</label>
-        <select>
+      <Campo>
+        <Label>Marca</Label>
+        <Select>
           <option value="">--- Seleecione ---</option>
           <option value="americano">Americano</option>
           <option value="europeo">Europeo</option>
           <option value="asiatico">Asiatico</option>
           <option value="otro">Otro</option>
-        </select>
-      </div>
+        </Select>
+      </Campo>
 
-      <div>
-        <label>Año</label>
-        <select>
+      <Campo>
+        <Label>Año</Label>
+        <Select>
           <option value="">--- Seleecione ---</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -30,16 +53,16 @@ const Formulario = () => {
           <option value="2012">2012</option>
           <option value="2011">2011</option>
           <option value="2010">2010</option>
-        </select>
-      </div>
+        </Select>
+      </Campo>
 
-      <div>
-        <label>Plan de Seguro</label>
-        <input type="radio" name="plan" value="basico" />
+      <Campo>
+        <Label>Plan de Seguro</Label>
+        <InputRadio type="radio" name="plan" value="basico" />
         Básico
-        <input type="radio" name="plan" value="completo" />
+        <InputRadio type="radio" name="plan" value="completo" />
         Completo
-      </div>
+      </Campo>
       <button type="button">Cotizar</button>
     </form>
   );
